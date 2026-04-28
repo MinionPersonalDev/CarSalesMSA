0d5b71e9806cfb02cc31af23c91266495fa2c2a8
Jayfunf <simh3077@gmail.com>
2026-04-28 10:55:33 +0900

    init: Spring MonoRepo

---
+++ contract-service/src/test/java/com/minion/contract_service/ContractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minion.contract_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
